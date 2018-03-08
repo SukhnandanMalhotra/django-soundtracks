b2d1e1367ba2671655daee63137559b471ca32c1
SukhnandanMalhotra <sukh.malhotra@gmail.com>
2018-03-08 18:58:15 +0530

    Add View

---
+++ project/tracks/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Tracks
+
+class TrackForm(forms.ModelForm):
+    class Meta:
+        model = Tracks
+        fields = ('track_name','audio_file',)
+        labels = {
+        'audio_file':'Audio File'
+        }
--- project/tracks/views.py
+++ project/tracks/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.http import HttpResponse, Http404, HttpResponseRedirect,
+from .forms.py import TrackForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from .models import Tracks
 
-# Create your views here.
+
+class AllTracksView(ListView):
+    model = Tracks
+    template_name = 'tracks/all_tracks.html'
+    context_object_name = 'audiofiles'
+
+class TrackUploadView(CreateView):
+    form_class = TrackForm
+    template_name = 'tracks/track_upload.html'
+
+    def form_valid(self, form):
+        audio = form.save(commit=False)
+        audio.save()
+        return  HttpResponseRedirect(reverse('home'))
