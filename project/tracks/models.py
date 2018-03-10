2e57a874719e6b7a0dc128238eb6bd25e3d7a0bd
SukhnandanMalhotra <sukh.malhotra@gmail.com>
2018-03-10 09:43:46 +0530

    Update validators

--- project/tracks/models.py
+++ project/tracks/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from .validators import validate_file_type
+from .validators import MimetypeValidator
 
 
 class Tracks(models.Model):
     track_name = models.CharField(max_length=250, blank=False,)
-    audio_file = models.FileField(upload_to='uploads/%Y/%m/%d/', validators=[validate_file_type])
+    audio_file = models.FileField(upload_to='uploads/%Y/%m/%d/', validators=[MimetypeValidator('.mp3')])
     uploaded_at = models.DateTimeField(auto_now_add=True)
