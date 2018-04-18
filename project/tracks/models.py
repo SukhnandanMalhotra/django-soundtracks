06326f54bd6f58089f9131be3fbae72e795dbb2d
SukhnandanMalhotra <sukh.malhotra@gmail.com>
2018-04-19 00:43:37 +0530

    Add requirements.txt

--- project/tracks/models.py
+++ project/tracks/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from .validators import validate_file_extension
 
-# Create your models here.
+
+class Tracks(models.Model):
+    track_name = models.CharField(max_length=250, blank=True)
+    audio_file = models.FileField(upload_to='uploads/%Y/%m/%d/', validators=[validate_file_extension])
+    uploaded_at = models.DateTimeField(auto_now_add=True)
---
+++ project/tracks/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .forms import TrackForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from .models import Tracks
+
+
+class AllTracksView(ListView):
+    model = Tracks
+    template_name = 'tracks/all_tracks.html'
+    context_object_name = 'audiofiles'
+
+
+class TrackUploadView(CreateView):
+    success_url=reverse_lazy('home')
+    form_class = TrackForm
+    template_name = 'tracks/track_upload.html'
+    context_object_name = 'form'
